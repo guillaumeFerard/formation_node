56f281057acb5e83918bf06264e4d59e0c4151a9
guillaume <48445936+guillaumeFerard@users.noreply.github.com>
2023-11-06 16:38:18 +0100

    parrallel

--- 12_parallel.js
+++ 12_parallel.js
@@ -14,9 +14,26 @@
 */
 
 //const {apiResponse} = require("./11_async");
+const axios = require("axios");
 
-const parallel = () => {
+const prom1 = axios.get("https://jsonplaceholder.typicode.com/todos/1")
+const prom2 = axios.get("https://jsonplaceholder.typicode.com/todos/2")
+const prom3 = axios.get("https://jsonplaceholder.typicode.com/todos/3")
 
+const promises = [prom1, prom2, prom3]
+
+
+const parallel = async () => {
+    const urls = ["https://jsonplaceholder.typicode.com/todos/1", "https://jsonplaceholder.typicode.com/todos/1", "https://jsonplaceholder.typicode.com/todos/1"]
+    const prom1 = axios.get(urls[0])
+    const prom2 = axios.get(urls[1])
+    const prom3 = axios.get(urls[2])
+    //urls.map((e) => axios)
+
+    let resp = await Promise.all([prom1, prom2, prom3]);
+    console.log(resp)
 }
+
+parallel()
 
 module.exports = {parallel};
