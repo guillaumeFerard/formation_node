4094e88c6a86e97282af8d4b95e1c3a388f8d5b4
guillaume <48445936+guillaumeFerard@users.noreply.github.com>
2023-11-06 10:49:40 +0100

    first

---
+++ 03_ternaireChain.js
@@ -0,0 +1,20 @@
+
+/**
+ * Utiliser les ternaires en les chainants, pour éviter les if elseif else
+ * 
+ * Renvoyer ">" si a supérieur à b
+ * Renvoyer "<" si a inférieur à b
+ * Renvoyer "=" si a égal à b
+ * 
+ * Contraintes:
+ *    - utiliser l'opérateur ternaire (if interdit)
+ */
+
+function ternaryChain(a, b) {
+
+    return a > b ? ">" : b > a ? ">" : "="
+
+}
+
+
+module.exports = ternaryChain;
---
+++ 04_arrow.js
@@ -0,0 +1,27 @@
+
+/**
+ * Déclarer une fonction fléchée qui renvoi l'argument + 1
+ * Le return est explicite
+ * 
+ * contrainte: 
+ *   - le mot clée "function" est interdis
+ */
+
+let arrow1 = (e) => {
+   return e + 1
+}
+
+/**
+ * Déclarer une fonction fléchée qui renvoi l'argument + 1
+ * Le return est implicite
+ * 
+ * contrainte: 
+ *   - le mot clée "function" est interdis
+ *   - le mot clée "return" est interdis
+ */
+
+let arrow2 = (e) => {
+    e + 1
+}
+
+module.exports = {arrow1, arrow2};
